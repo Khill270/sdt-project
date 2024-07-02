22c434fa2805418ec2aa7ef78b73cea6121b8aa1
Khill270 <khillyahmeen12@gmail.com>
2024-07-02 18:51:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+filename = '23-24 NBA Player Stats - Regular.csv'
+encoding = 'latin-1'
+
+try:
+    nba_stats_df = pd.read_csv(filename, encoding=encoding, delimiter=';')
+    print(nba_stats_df.head(10))
+except UnicodeDecodeError as e:
+    print(f"UnicodeDecodeError: {e}")
+
+st.header('Exploring NBA Player Stats')
+
+if st.checkbox('Show Advanced Stats'):
+  fig_hist = px.histogram(df, x='PTS', title='Points per Game Distribution')
+    st.plotly_chart(fig_hist)
+  fig_scatter = px.scatter(df, x='AST', y='TRB', color='Pos', hover_name='Player', title='Assists vs. Rebounds')
+    st.plotly_chart(fig_scatter)
